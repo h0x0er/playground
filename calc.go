a170ba2be0725ff8301da18a2e010bcd8256de33
jatin <84621253+h0x0er@users.noreply.github.com>
2024-02-12 16:06:27 +0530

    Update calc.go

--- calc.go
+++ calc.go
@@ -1,8 +1,22 @@
 package main
 
 import "fmt"
+import "os/exec"
+
+func isEcaptureRunning() bool {
+	resp, err := exec.Command("/bin/sh", "sudo pidof ecapture").Output()
+	if err != nil {
+		return false
+	}
+	out := string(resp)
+	// fmt.Printf("out: %v\n", out)
+	return len(strings.TrimSpace(out)) > 0
+
+}
 
 func main(){
 
   fmt.Println("hello world !")
+  fmt.Println("IsEcaptureRunning: %v\n", isEcaptureRunning())
+  
 }
