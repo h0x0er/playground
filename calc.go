2d560a620290537579b06fe7a2d0867580496bee
jatin <84621253+h0x0er@users.noreply.github.com>
2024-02-28 13:51:16 +0530

    Update calc.go

--- calc.go
+++ calc.go
@@ -26,6 +26,31 @@ func isEcaptureInstalled() bool {
 	return true
 }
 
+func getRunnerVersion() string {
+
+	version := "*"
+
+	output, err := exec.Command("/bin/sh", "-c", `ps $(pidof Runner.Listener) | grep -ioE "/.+"`).Output()
+	if err != nil {
+		return version
+	}
+
+	if len(output) == 0 {
+		return version
+	}
+	// output = []byte("/home/runner/runners/2.313.0/bin/Runner.Listener run")
+	outputParts := strings.Split(string(output), " ")
+	if len(outputParts) < 2 {
+		return version
+	}
+
+	runnerListenerPath := outputParts[0]
+	pathParts := strings.Split(runnerListenerPath, "/")
+
+	version = pathParts[4]
+
+	return version
+}
 
 func main(){
 
@@ -33,5 +58,7 @@ func main(){
 	
   fmt.Printf("Ecapture Installed: %v\n", isEcaptureInstalled())
   fmt.Printf("IsEcaptureRunning: %v\n", isEcaptureRunning())
+  fmt.Printf("RunnerVersion: %v\n", getRunnerVersion())
+	
   
 }
